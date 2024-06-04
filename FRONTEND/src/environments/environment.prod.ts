bda5b7509485f1c1dcd8b6e37f420bb6531a0b8e
chaditroudi <troudishedy6@gmail.com>
2024-06-04 15:35:14 +0200

    delete env prod

--- FRONTEND/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-   API_URL:'https://demo.pestras.com/api',
-  //API_URL:'http://localhost:3200/api',
-  SOCKET_URL: 'https://demo.pestras.com'
-
-};
