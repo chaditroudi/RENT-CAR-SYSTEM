f96bf5e3c22dbb7a7803982404efcf41371090a5
chaditroudi <troudishedy6@gmail.com>
2024-06-04 15:31:53 +0200

    stop tracking env prod

--- FRONTEND/src/environments/environment.prod.ts
+++ FRONTEND/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
    API_URL:'https://demo.pestras.com/api',
   //API_URL:'http://localhost:3200/api',
-  SOCKET_URL:'http://localhost:3200'
+  SOCKET_URL: 'https://demo.pestras.com'
 
 };
